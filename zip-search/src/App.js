72bbd8253ff01fd04e8b350a6e729d393983051e
cchromak <cchromak@gmail.com>
2020-09-28 12:18:57 -0400

    Created array and stored json

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,25 +3,73 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <div >
+    <ul>
+      <li>State: {props.state}</li>
+      <li>Location: {props.location}</li>
+      <li>Population: {props.city}</li>
+    </ul>
+  </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+  <div>
+    Enter a Zip code:
+    <input type="text" onChange={ props.onChange } />
+    <p>You entered: </p>
+  </div>);
 }
 
 
+//onchange e target returns value in input to console // onChange={(e) => console.log(e.target.value)}
+
+
 class App extends Component {
+
+  state = {
+    userInputValue: "",
+    cities: [],
+  }
+
+  handleZipChange(event) {
+    let url = 'http://ctp-zip-api.herokuapp.com/zip/' + event.target.value;
+    console.log(event.target.value);
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          cities: json,
+        })
+      })
+  }
+
+
+  // create an array of <City /> elements,
+
   render() {
+    const data = this.state.cities;
+    const dataArray = [];
+    for(let i = 0; i < data.length; i++) {
+      const curr = data[i];
+      dataArray.push(
+        <City
+        city={curr['City']}
+        state={curr["State"]}
+        location={curr['Lat'] + ", " + curr['Long']}
+        />
+      )
+    }
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField onChange={(e) => this.handleZipChange(e)}/>
         <div>
-          <City />
-          <City />
+          {dataArray}
         </div>
       </div>
     );
