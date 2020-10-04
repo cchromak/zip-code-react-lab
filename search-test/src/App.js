8e192dace7dfaf09c5d7b6c231820597c88515fc
cchromak <cchromak@gmail.com>
2020-10-04 13:10:52 -0400

    testing city search functionality in search-test

--- search-test/src/App.js
+++ search-test/src/App.js
@@ -1,74 +1,73 @@
 import React, { Component } from 'react';
-
 import './App.css';
 
-function InputZip(props) {
-  return (
+function InputCity(props) {
+  return(
     <div>
-      <input type="text" onChange={ props.zip }></input>
+      Enter a city name<input onChange= { props.onChange }></input>
     </div>
   )
 }
 
-function City(props) {
-  return (
-    <div>
-      <ul>
-        <li>Name: { props.city }</li>
-        <li>Population: { props.population }</li>
-        <li>Location: { props.location } </li>
-      </ul>
-    </div>
-  )
-}
+
+
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cities: [],
+      cityName: "",
+      zipsArray: [],
+      cityArray: [],
     }
-    this.handleChange = this.handleChange.bind(this);
-  }
+  };
 
-  handleChange(event) {
-    let zipCode = event.target.value;
-    if (zipCode.length === 5) {
-      let url = 'http://ctp-zip-api.herokuapp.com/zip/' + zipCode;
-      fetch(url)
+  handleCityName(event) {
+    let currName = event.target.value.toUpperCase();
+    let cityUrl= "http://ctp-zip-api.herokuapp.com/city/" + currName;
+    let zipUrl = "http://ctp-zip-api.herokuapp.com/zip/";
+    let promiseZips = fetch(cityUrl)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          zipsArray: json,
+        })
+      })
+    .catch(this.setState({
+      zipsArray: [],
+    }));
+    
+    
+    let allZipCodes = this.state.zipsArray;
+    for (let i = 0; i < allZipCodes.length; i++) {
+      let zipUrl = "http://ctp-zip-api.herokuapp.com/zip/" + allZipCodes[i];
+      fetch(zipUrl)
         .then(response => response.json())
         .then(json => {
           this.setState({
-            cities: json,
+            cityArray: this.state.cityArray.push(json),
           })
         })
-    } else {
-      this.setState({
-        cities: [],
-      })
+      console.log(zipUrl);
     }
-  }
 
+  //   Promise.all([promiseZips]).then(values =>
+  //     console.log(values))
+  //     fetch(zipUrl + values)
+  //     .then(response => response.json())
+  //     .then(data => console.log(data)));
+   }
 
   render() {
-    let cityArray = this.state.cities;
-    let cityDisplay = [];
-    for (let i = 0; i < cityArray.length; i++) {
-      cityDisplay.push(
-        <City 
-        city={ cityArray[i]['City']}
-        population={ cityArray[i]['EstimatedPopulation']}
-        location={ cityArray[i]['Lat'] + ", " + cityArray[i]['Long']}
-        />
-      )
-    }
     return (
       <div>
-        <InputZip zip={(e) => this.handleChange(e)} />
-        <p> { cityDisplay.length === 0 ? "Please enter a zip code" : cityDisplay }</p>
+        <InputCity onChange={ (e) => this.handleCityName(e) } />
+        <p> { this.state.cityArray } </p>
       </div>
     )
   }
 }
+
 
 export default App;
